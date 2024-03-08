004e225b213a4e176fb7d3c9656043909b9cf58b
Timmy Jose <zoltan.jose@gmail.com>
2024-03-08 19:38:16 +0530

    Initia working version (with W3mButton).

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import '@walletconnect/react-native-compat'
+import { WagmiConfig } from 'wagmi'
+import { mainnet, polygon, arbitrum } from 'viem/chains'
+import { createWeb3Modal, defaultWagmiConfig, Web3Modal } from '@web3modal/wagmi-react-native'
+import ConnectView from './components/ConnectView'
+
+const projectId = 'a12eedba55522ed87542343db1629d8c'
+
+const metadata = {
+  name: 'Wallet Connect Demo App',
+  description: 'Wallet Connect Demo Test App',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886'],
+  redirect: {
+    native: '',
+    universal: ''
+  }
+}
+
+const chains = [mainnet, polygon, arbitrum]
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+
+createWeb3Modal({
+  projectId,
+  chains,
+  wagmiConfig
+})
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <WagmiConfig config={wagmiConfig}>
+      <ConnectView />
+      <Web3Modal />
+    </WagmiConfig>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/ConnectView.tsx
@@ -0,0 +1,10 @@
+import { W3mButton } from '@web3modal/wagmi-react-native'
+
+export default function ConnectView() {
+  return (
+    <>
+      <W3mButton />
+    </>
+  )
+
+}
